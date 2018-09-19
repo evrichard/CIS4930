dc5d78af3855f054443abe2e00352c33f99c484e
Unknown <erichard@ufl.edu>
2018-09-18 23:24:38 -0400

    Finished Assignment 1
    Needs custom Maven run configuration, you can then test using java -jar webAppMini1-0.1.0

--- music/src/main/java/dao/AlbumDAO.java
+++ music/src/main/java/dao/AlbumDAO.java
@@ -15,13 +15,27 @@ public class AlbumDAO {
 
     public Album createAlbum(Album album){
         //TODO: Implement this CRUD function
+        //Here we create an album with the two required feilds
+        int albumId = album.getId();
+        String albumTitle = album.getTitle();
+        this.jdbcTemplate.update("INSERT INTO albums (title, id) VALUES (?,?)", albumTitle, albumId);
         return album;
     }
 
     public Album getAlbum(int id){
         Album album = new Album(id, "");
         //TODO: Implement this CRUD function
-        //Get album and set tracks using getTracksByAlbumId(id) in TracksDAO
+        //Get album and set tracks using getTracksByAlbumId(id) in TracksDAOINSERT INTO albums (id, title)
+        //We used a method similar to the one provided, but filled in for the required fields.
+        TrackDAO track = new TrackDAO(jdbcTemplate);
+
+        this.jdbcTemplate.query(
+                "SELECT * FROM albums", new Object[] { },
+                (rs, rowNum) -> new Album(rs.getInt("id"), rs.getString("title"))
+        ).forEach(albumInteration -> {
+            album.setTitle(albumInteration.getTitle());
+            album.setTracks(track.getTracksByAlbumId(id));
+        });
         return album;
     }
 
@@ -37,15 +51,20 @@ public class AlbumDAO {
 
     public Album updateAlbum(Album album){
         //TODO: Implement this CRUD function
+        //Here we update an album with simple SQL
+        String albumTitle = album.getTitle();
+        int albumId = album.getId();
+        String updateQ = "UPDATE albums SET title = ? WHERE id = ? ";
+        this.jdbcTemplate.update(updateQ, albumTitle, albumId);
         return album;
     }
 
     public boolean deleteAlbum(Album album){
-        boolean success = false;
         //TODO: Implement this CRUD function
-        return success;
+        //Here we delete an album with simple SQL
+        int albumId = album.getId();
+        String delQ = "DELETE FROM albums where id = ?";
+        this.jdbcTemplate.update(delQ, albumId);
+        return true;
     }
-
-
-
 }
--- music/src/main/java/dao/TrackDAO.java
+++ music/src/main/java/dao/TrackDAO.java
@@ -17,24 +17,43 @@ public class TrackDAO {
 
     public Track createTrack(Track track){
         //TODO: Implement this CRUD function
+        //This function will create the track with the required variables
+        String trackTitle = track.getTitle();
+        int trackId = track.getId();
+        int albumTitle = track.getAlbumId();
+        this.jdbcTemplate.update("INSERT INTO tracks (title, id, album) VALUES (?,?,?)", trackTitle, trackId, albumTitle);
         return track;
     }
 
     public Track getTrack(int id){
         Track track = new Track(id);
         //TODO: Implement this CRUD function
+        //This function will get the track and fill the the three required variables. We don't need to instantiate a
+        //album object
+        this.jdbcTemplate.query(
+                "SELECT * FROM tracks", new Object[] { },
+                (rs, rowNum) -> new Track(rs.getInt("id"), rs.getString("title"), rs.getInt("album"))
+        ).forEach(trackIteration -> { track.setId(trackIteration.getId());
+            track.setTitle(trackIteration.getTitle());
+            track.setAlbumId(trackIteration.getAlbumId());
+            //need to set all three variables required by the Track class
+        });
         return track;
     }
 
     public Collection<Track> getAllTracks(){
         Collection<Track> tracks = new ArrayList<Track>();
         //TODO: Implement this CRUD function
+        //Here we get all the tracks, similar to how we did in AlbumDAO
+        this.jdbcTemplate.query(
+                "SELECT * FROM tracks", new Object[] { },
+                (rs, rowNum) -> new Track(rs.getInt("id"), rs.getString("title"), rs.getInt("album"))
+        ).forEach(trackIteration -> tracks.add(trackIteration));
         return tracks;
     }
 
     public Collection<Track> getTracksByAlbumId(int albumId){
         Collection<Track> tracks = new ArrayList<Track>();
-
         this.jdbcTemplate.query(
                 "SELECT id, title FROM tracks WHERE album = ?", new Object[] { albumId },
                 (rs, rowNum) -> new Track(rs.getInt("id"), rs.getString("title"),albumId)
@@ -44,13 +63,22 @@ public class TrackDAO {
     }
     public Track updateTrack(Track track){
         //TODO: Implement this CRUD function
+        //Here we update the tracks with simple SQL
+        String trackTitle = track.getTitle();
+        int trackId = track.getId();
+        String updateQ = "UPDATE tracks SET title = ? WHERE id = ?";
+        this.jdbcTemplate.update(updateQ, trackTitle, trackId);
+        //assuming you can't update the title
         return track;
     }
 
     public boolean deleteTrack(Track track){
-        boolean success = false;
         //TODO: Implement this CRUD function
-        return success;
+        //Here we delete the track with simple SQL
+        int trackId = track.getId();
+        String delQ = "DELETE FROM tracks where id = ?";
+        this.jdbcTemplate.update(delQ, trackId);
+        return true;
     }
 
 }
